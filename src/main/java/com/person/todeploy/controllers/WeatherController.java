b065077715c9674e96fb5303105ee3465ebb3d71
navas <gabrielnavas@email.com>
2023-09-07 15:46:52 -0300

    add param query to say now or not

--- src/main/java/com/person/todeploy/controllers/WeatherController.java
+++ src/main/java/com/person/todeploy/controllers/WeatherController.java
@@ -2,16 +2,20 @@ package com.person.todeploy.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "weather")
 public class WeatherController {
 
     @GetMapping
-    public ResponseEntity<Object> getWeather (){
-        return ResponseEntity.status(HttpStatus.OK).body("Very hot!!");
+    public ResponseEntity<Object> getWeather (
+            @RequestParam(value = "now", defaultValue = "false") boolean now
+    ){
+        String weather = "Very hot all day";
+        if(now) {
+            weather = "Very hot today!!";
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(weather);
     }
 }
