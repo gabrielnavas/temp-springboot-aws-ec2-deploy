374b5e7b49b851332049894fcd2b7a5d2f99e443
navas <gabrielnavas@email.com>
2023-09-07 12:50:49 -0300

    simple weather api with get weather now

---
+++ src/main/java/com/person/todeploy/TodeployApplication.java
@@ -0,0 +1,13 @@
+package com.person.todeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/person/todeploy/TodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.todeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
