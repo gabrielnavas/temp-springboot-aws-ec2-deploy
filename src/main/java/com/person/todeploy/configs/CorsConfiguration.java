ca8e61ad59deebd063162ede9643c488bf3a8e8c
navas <gabrielnavas@email.com>
2023-09-07 14:28:05 -0300

    add cors

---
+++ src/main/java/com/person/todeploy/configs/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.person.todeploy.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
